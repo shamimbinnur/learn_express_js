a2b8c72d19347b133cb5396f51209107760c0e5c
shamim <sbnsbn00@gmail.com>
2021-03-20 12:53:23 +0600

    Added middleware function

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
+// Author: Shamim Bin Nur
+// Discription: Rest api practice with Express JS
+// Date: 20-03-2021
+
+
 const express = require("express");
 const app = express();
 const students = require('./database/students');
 
 //dependencies
 const PORT = process.env.PORT || 5000
+
+
+//creating middleware function that will when get any request
+const demoMiddleware = (req, res, next) => {
+    console.log("Demo middleware run!");
+    next();
+}
+
+// Initializing middleware so that it works
+app.use(demoMiddleware);
+
+
 
 app.get('/api/students', (req, res)=> {
     //sending response as Json format
