146f0ac496256aab91b67fc2242bd7da6080c95b
shamim <sbnsbn00@gmail.com>
2021-03-21 11:03:17 +0600

    added template engnine

--- index.js
+++ index.js
@@ -2,9 +2,10 @@
 // Description: Rest api practice with Express JS
 // Date: 20-03-2021
 
-
+const ejs = require('ejs');
 const express = require("express");
 const app = express();
+
 
 
 //defining port number
@@ -21,12 +22,26 @@ const studentRouter = require('./routes/studentRouter');
 // Initializing middlewares so that it works 
 app.use(demoMiddleware,secondDemoMiddleware);
 
+//Initializing template engine
+app.set("view engine", ejs);
+
 //setting node built-in middleware functions to parse json data sent from cliend
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
 
+
 //set studentRouters
 app.use('/students',studentRouter)
+
+//Usinging template engine
+app.get('/html', (req, res)=>{
+    res.render("home.ejs", { students:
+        [ 
+            {name:"Shamim Bin Nur", id: "011"},
+            {name:"Abdur Rahim Bin Nur", id: "022"},
+        ]
+    })
+})
 
 
 app.listen(PORT, ()=> {
